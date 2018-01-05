d5ed2b4a92311fce083b2863f7a17d3764ceb409
shubh2565 <shubh2565@users.noreply.github.com>
2018-01-05 11:25:26 +0530

    Add files via upload

---
+++ web_map1.py
@@ -0,0 +1,18 @@
+import folium
+import pandas
+
+data = pandas.read_csv('Volcanoes_USA.txt')
+lat = list(data['LAT'])
+lon = list(data['LON'])
+elev = list(data['ELEV'])
+
+map = folium.Map(location = [38, -99], zoom_start = 4, tiles = 'Mapbox Bright')
+
+fg = folium.FeatureGroup(name = 'My Map')
+
+for lt, ln, el in zip(lat, lon, elev):
+	fg.add_child(folium.Marker(location = [lt, ln], popup = str(el) + ' m', icon = folium.Icon(color = 'green')))
+
+map.add_child(fg)
+
+map.save("Map1.html")
