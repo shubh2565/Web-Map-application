6c85b71625c889c2f5e0e0dad2b42246df578663
shubh2565 <shubh2565@users.noreply.github.com>
2018-01-10 17:47:32 +0530

    Update app_web_map.py

--- app_web_map.py
+++ app_web_map.py
@@ -1,11 +1,13 @@
 import folium
 import pandas
 
-data = pandas.read_csv('Volcanoes_USA.txt')
+data = pandas.read_csv('Volcanoes_USA.txt')  # creates a dataframe
 lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
 
+
+# function to give assign colors to the markers on the basis of elevation 
 def icon_color(elevation):
 	if elevation <= 2000:
 		return 'green'
@@ -16,23 +18,23 @@ def icon_color(elevation):
 	else:
 		return 'red'
 
-map = folium.Map(location = [38, -99], zoom_start = 4, tiles = 'Mapbox Bright')
+map = folium.Map(location = [38, -99], zoom_start = 4, tiles = 'Mapbox Bright')  # creates a map object 
 
-fgV = folium.FeatureGroup(name = 'Volcanoes USA')
+fgV = folium.FeatureGroup(name = 'Volcanoes USA') # creates a feature group. We can add child directly to map object but this is more refined way.
 
-for lt, ln, el in zip(lat, lon, elev):
+for lt, ln, el in zip(lat, lon, elev):     # zip is used when simultaneously iterating through more than one list
 	fgV.add_child(folium.CircleMarker(location = [lt, ln], popup = str(el) + ' m', radius=6, color = icon_color(el), fill = True, fill_opacity = 0.75, fill_color = icon_color(el)))
 
-fgP = folium.FeatureGroup(name = 'World Population')
+fgP = folium.FeatureGroup(name = 'World Population')  # another feature group
 
 fgP.add_child(folium.GeoJson(data = open('world.json', 'r', encoding = 'utf-8-sig').read(), 
-style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000    # lamda method is used to assign different colors to different population range
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 30000000
 else 'purple' if 30000000 <= x['properties']['POP2005'] < 50000000
 else 'red'}))
 
 map.add_child(fgV)
 map.add_child(fgP)
-map.add_child(folium.LayerControl())
+map.add_child(folium.LayerControl()) 
 
 map.save("WebMap.html")
